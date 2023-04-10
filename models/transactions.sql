47b468a869ad4f6899b20eca8988010fd18b403c
Rich Leung <kleung.hkg@gmail.com>
2023-04-10 19:25:07 +0000

    added purchase Period. Assuming monthly purchasing frequency.

--- models/transactions.sql
+++ models/transactions.sql
@@ -5,20 +5,21 @@
 }}
 
 select 
-  OG.CustomerID
-  , OG.Country
-  , OG.InvoiceDate
-  , OG.InvoiceNo
-  , OG.StockCode
-  , OG.Description
-  , OG.Quantity
-  , OG.UnitPrice
-  , (OG.UnitPrice * OG.Quantity) as TotalPrice
-  , fp.Init_Purchase_Date
-  , fp.Init_Purchase_YrMo
-  , fp.Init_Purchase_YrQtr
+    OG.CustomerID
+    , OG.Country
+    , OG.InvoiceDate
+    , date_diff(date(OG.InvoiceDate), date(fp.Init_Purchase_Date), MONTH) as Period
+    , OG.InvoiceNo
+    , OG.StockCode
+    , OG.Description
+    , OG.Quantity
+    , OG.UnitPrice
+    , (OG.UnitPrice * OG.Quantity) as TotalPrice
+    , fp.Init_Purchase_Date
+    , fp.Init_Purchase_YrMo
+    , fp.Init_Purchase_YrQtr
 from `landingzone.ecommerce_data` as OG
 left join `dbtPROD___sample_eCommerce_data.initial_purchase` as fp
-  on OG.CustomerID = fp.CustomerID
+    on OG.CustomerID = fp.CustomerID
 where
-  (OG.CustomerID is not null)
+    (OG.CustomerID is not null)
