ef38ab7eb23108f9ed8aa21b39872aa2685729ce
Rich Leung <kleung.hkg@gmail.com>
2023-04-10 15:59:28 +0000

    Add customer's  initial purchase date model, tests, docs

---
+++ models/initial_purchase.sql
@@ -0,0 +1,23 @@
+-- override dbt_project.yml and persist result as table
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+
+with Init_Purchase as (
+  select 
+    CustomerID
+    , min(InvoiceDate) as Init_Purchase_Date
+  from `landingzone.ecommerce_data` 
+  where (CustomerID is not null)
+  group by 1
+)
+
+select 
+  CustomerID
+  , Init_Purchase_Date
+  , date_trunc(Init_Purchase_Date, month) as Init_Purchase_YrMo
+  , date_trunc(Init_Purchase_Date, quarter) as Init_Purchase_YrQtr
+from Init_Purchase
